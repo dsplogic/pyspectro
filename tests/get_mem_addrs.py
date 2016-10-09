eb7321fee0036738767fc9a4a46c2307584de2ad
mikebabst <mikebabst@users.noreply.github.com>
2016-10-09 07:40:40 -0400

    Fixing DDR memory index files

--- tests/get_mem_addrs.py
+++ tests/get_mem_addrs.py
@@ -1,3 +1,18 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2016, DSPlogic, Inc.  All Rights Reserved.  
+# 
+# RESTRICTED RIGHTS
+# Use of this software is permitted only with a software license agreement.
+#
+# Details of the software license agreement are in the file LICENSE.txt, 
+# distributed with this software.
+#------------------------------------------------------------------------------
+""" Get memory addresses
+
+This script computes the mapping of FFT bins to memory addresses
+and saves the results to files (one each for DDRA/B).
+
+"""
 
 import numpy as np
 
@@ -6,5 +21,5 @@ from pyspectro.drivers.Spectrometer import MemoryConverter
 mc = MemoryConverter(32768)
 
 #: Save to file
-np.savetxt("idx_ddra.csv", mc._ddra_indices, delimiter=",") 
-np.savetxt("idx_ddrb.csv", mc._ddra_indices, delimiter=",") 
+np.savetxt("idx_ddra.txt", mc._ddra_indices, fmt = '%i', delimiter=",") 
+np.savetxt("idx_ddrb.txt", mc._ddrb_indices, fmt = '%i',delimiter=",") 
