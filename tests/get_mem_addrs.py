a11a959c72152e7f37f815e6068d83427bf132cf
mikebabst <mikebabst@users.noreply.github.com>
2019-03-01 11:37:50 -0500

    Update memory indices for all FFT sizes.

--- tests/get_mem_addrs.py
+++ tests/get_mem_addrs.py
@@ -18,8 +18,20 @@ import numpy as np
 
 #: Create a memory converter object to pre-compute index map
 from pyspectro.drivers.Spectrometer import MemoryConverter
-mc = MemoryConverter(32768)
 
-#: Save to file
-np.savetxt("idx_ddra.txt", mc._ddra_indices, fmt = '%i', delimiter=",") 
-np.savetxt("idx_ddrb.txt", mc._ddrb_indices, fmt = '%i',delimiter=",") 
+mc = MemoryConverter(32768, False)
+np.savetxt("memidx_n32k_real_ddra.txt", mc._ddra_indices, fmt = '%i', delimiter=",") 
+np.savetxt("memidx_n32k_real_ddrb.txt", mc._ddrb_indices, fmt = '%i',delimiter=",") 
+
+mc = MemoryConverter(16384, True)
+np.savetxt("memidx_n16k_complex_ddra.txt", mc._ddra_indices, fmt = '%i', delimiter=",") 
+np.savetxt("memidx_n16k_complex_ddrb.txt", mc._ddrb_indices, fmt = '%i',delimiter=",") 
+
+mc = MemoryConverter(8192, True)
+np.savetxt("memidx_n8k_complex_ddra.txt", mc._ddra_indices, fmt = '%i', delimiter=",") 
+np.savetxt("memidx_n8k_complex_ddrb.txt", mc._ddrb_indices, fmt = '%i',delimiter=",") 
+
+mc = MemoryConverter(4096, True)
+np.savetxt("memidx_n4k_complex_ddra.txt", mc._ddra_indices, fmt = '%i', delimiter=",") 
+np.savetxt("memidx_n4k_complex_ddrb.txt", mc._ddrb_indices, fmt = '%i',delimiter=",") 
+
